4d42dc51e5abbd6f699e36b13f0c77a3c35c434d
Aquiles <aquilesnetto2002@gmail.com>
2025-04-28 15:44:21 -0300

    Viewsets em django Rest Framework

--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -4,11 +4,15 @@ from product.models.category import Category
 
 class CategorySerializer(serializers.ModelSerializer):
 
+
+
     class Meta:
         model = Category
         fields = [
             'title',
             'slug',
             'description',
-            'active'
+            'active',
     ]
+
+    extra_kwargs = {'slug': {'required': False}}
