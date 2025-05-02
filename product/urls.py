867a2038b65946944a6ac6b2951eb4a5f1942fda
Aquiles <aquilesnetto2002@gmail.com>
2025-05-02 15:48:43 -0300

    conatiners em docker

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include 
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 
 from product import viewset
 
@@ -10,6 +9,5 @@ router.register(r'category', viewset.Category_ViewSet, basename='category')
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('', include(router.urls))
 ]
