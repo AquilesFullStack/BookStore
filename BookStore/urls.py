43909bcf9e152dbc0a9866b0bb46a8b164d3fa23
Aquiles <aquilesnetto2002@gmail.com>
2025-05-07 10:43:25 -0300

    feat: add template-views-helloWolrd

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
+    path("update_server/", views.update, name="update")
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls'))
 ]
