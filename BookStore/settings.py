cd4bdaba3afff248ab4cc31d3cdd717756040336
Aquiles <aquilesnetto2002@gmail.com>
2025-05-07 12:18:25 -0300

    Corrige erro de sintaxe

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "BookStore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "BookStore", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
